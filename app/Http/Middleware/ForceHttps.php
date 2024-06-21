a6c2e6b516a1191db35621c146baea85b8a4d935
darkzpvp <victor1val@hotmail.es>
2024-06-21 13:54:06 +0200

    Nuevo

--- app/Http/Middleware/ForceHttps.php
+++ app/Http/Middleware/ForceHttps.php
@@ -1,13 +1,20 @@
 <?php
+
 namespace App\Http\Middleware;
+
 use Closure;
+
 class ForceHttps
 {
-   public function handle($request, Closure $next)
-   {
-       if (!$request->secure()) {
-           return redirect()->secure($request->getRequestUri());
-       }
-       return $next($request);
-   }
+    public function handle($request, Closure $next)
+    {
+        // Verificar si la solicitud no es segura (HTTP)
+        if (!$request->secure()) {
+            // Redirigir a HTTPS
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        // Continuar con la solicitud si ya está en HTTPS
+        return $next($request);
+    }
 }
