02fd06e4088aa8b367c9b5ba155b5208a4abf638
darkzpvp <victor1val@hotmail.es>
2024-06-21 14:38:30 +0200

    Nuevo

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -16,21 +16,26 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
-            // Obtener la URL base del frontend desde la configuración
-            $frontendUrl = config('app.frontend_url');
-
-            // Reemplazar la parte /api de la URL y eliminar el puerto si está presente
-            $spaUrl = str_replace('/api', '', $url);
-            $spaUrl = preg_replace('/:[0-9]+/', '', $spaUrl);
-
-            // Construir la URL completa para el frontend
-            $frontendVerificationUrl = rtrim($frontendUrl, '/') . $spaUrl;
-
+            // URL base específica del frontend que deseas utilizar
+            $frontendUrl = 'https://forst-ai.vercel.app/';
+        
+            // Obtener los parámetros de la URL generada por Laravel
+            $params = parse_url($url);
+        
+            // Eliminar la parte /api de la URL
+            $path = str_replace('/api', '', $params['path']);
+        
+            // Eliminar el puerto si está presente
+            $path = preg_replace('/:[0-9]+/', '', $path);
+        
+            // Construir la nueva URL del frontend con el path y los parámetros
+            $spaUrl = $frontendUrl . ltrim($path, '/') . '?' . $params['query'];
+        
             return (new \Illuminate\Notifications\Messages\MailMessage)
                 ->subject('Verifica la dirección de correo electrónico')
                 ->greeting('Hola ' . $notifiable->name . ',')
                 ->line('¡Pincha en el botón y verifica la cuenta!')
-                ->action('Verificar email', $frontendVerificationUrl)
+                ->action('Verificar email', $spaUrl)
                 ->salutation('Gracias, ' . config('app.name'));
         });
     }
