698659f8211876799ba88661680da5569f71e202
darkzpvp <victor1val@hotmail.es>
2024-06-21 13:49:04 +0200

    Nuevo

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -16,17 +16,21 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
-            // Eliminar la parte /api de la URL
+            // Obtener la URL base del frontend desde la configuración
+            $frontendUrl = config('app.frontend_url');
+
+            // Reemplazar la parte /api de la URL y eliminar el puerto si está presente
             $spaUrl = str_replace('/api', '', $url);
-            
-            // Eliminar el puerto si está presente
             $spaUrl = preg_replace('/:[0-9]+/', '', $spaUrl);
-        
+
+            // Construir la URL completa para el frontend
+            $frontendVerificationUrl = rtrim($frontendUrl, '/') . $spaUrl;
+
             return (new \Illuminate\Notifications\Messages\MailMessage)
                 ->subject('Verifica la dirección de correo electrónico')
                 ->greeting('Hola ' . $notifiable->name . ',')
                 ->line('¡Pincha en el botón y verifica la cuenta!')
-                ->action('Verificar email', $spaUrl)
+                ->action('Verificar email', $frontendVerificationUrl)
                 ->salutation('Gracias, ' . config('app.name'));
         });
     }
