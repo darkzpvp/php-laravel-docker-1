26bf203e3605db6830e5b96d52228f4f1f18bcd7
darkzpvp <victor1val@hotmail.es>
2024-06-21 02:23:50 +0200

    Nuevo

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,10 +19,9 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'name', 'email', 'password', 'ip_address',
     ];
+    
 
     /**
      * The attributes that should be hidden for serialization.
